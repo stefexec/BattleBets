a4a77662eaade670348338c578b2080140eebb78
BuildTools <unconfigured@null.spigotmc.org>
2022-08-13 22:22:56 +0200

    added particle effects

--- src/main/java/me/bananababoo/battlebets/SubCommands/Kits.java
+++ src/main/java/me/bananababoo/battlebets/SubCommands/Kits.java
@@ -1,10 +1,18 @@
 package me.bananababoo.battlebets.SubCommands;
 
 import me.bananababoo.battlebets.Arena;
+import me.bananababoo.battlebets.BattleBets;
 import me.bananababoo.battlebets.TeamM;
 import me.bananababoo.battlebets.Utils.StorageUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.io.File;
 
 public class Kits {
     public static void giveKit(String redKit, String blueKit) {
@@ -15,9 +23,22 @@ public class Kits {
         for (Player p : Bukkit.getOnlinePlayers()) {
             if(TeamM.Team(p).equals("red")){
                 Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "cmi kit " + redKit + " " + p.getName());
+
+                // particles
+                p.getWorld().spawnParticle(Particle.REDSTONE, p.getLocation(), 25, 1, 2, 1, new Particle.DustOptions(org.bukkit.Color.fromRGB(255, 0, 0), 2));
             }
             else if (TeamM.Team(p).equals("blue")){
                 Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "cmi kit " + blueKit + " " + p.getName());
+
+                // BEEF TEST
+                p.getInventory().addItem(new ItemStack(Material.COOKED_BEEF));
+
+                // create new .yml to store kit data
+                // IDK if it is even needed if we use CMI for items
+                // FileConfiguration bb_kits = YamlConfiguration.loadConfiguration(new File(BattleBets.getDataFolder(), "bb_kits.yml"));
+
+                // ill just add some particles for now lol
+                p.getWorld().spawnParticle(Particle.REDSTONE, p.getLocation(), 25, 1, 2, 1, new Particle.DustOptions(org.bukkit.Color.fromRGB(0, 0, 255), 2));
             }
         }
     }
